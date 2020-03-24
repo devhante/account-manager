a2379074f45eb74518662dd3d8974e8146b1ca9a
Kyungwook <Cho>
2020-03-24 17:24:22 +0900

    can save and load with localStorage

--- src/stores/account.ts
+++ src/stores/account.ts
@@ -1,5 +1,5 @@
-import { observable } from 'mobx';
-import { ManagerSerializer} from '../serializer';
+import { observable, action } from 'mobx';
+import { AccountSerializer} from '../serializer';
 import RootStore from './root';
 
 export default class AccountStore {
@@ -8,5 +8,21 @@ export default class AccountStore {
 
     constructor(rootStore: RootStore) {
         this.rootStore = rootStore;
+        this.load();
+    }
+
+    @action
+    public load = () => {
+        if(localStorage.getItem('account') !== null) {
+            let text: string = localStorage.getItem('account') as string;
+            let json = JSON.parse(text);
+            for(let item of json) {
+                this.AccountList.push(new AccountSerializer(item.id, item.password, item.name, item.position, item.isUsing));
+            }
+        }
+    }
+
+    public save = () => {
+        localStorage.setItem('account', JSON.stringify(this.AccountList));
     }
 }
